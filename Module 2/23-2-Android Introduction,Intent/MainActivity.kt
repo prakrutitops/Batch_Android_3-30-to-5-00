d2f6b5b6f6ed777e2b516cfa2e2868e364a8c594
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-02-23 11:24:43 +0530

    Add files via upload

---
+++ Module 2/23-2-Android Introduction,Intent/MainActivity.kt
@@ -0,0 +1,71 @@
+package tops.firstapp
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity()
+{
+    //Declare
+
+    lateinit var t1:TextView
+    lateinit var t2:TextView
+    lateinit var t3:TextView
+    lateinit var t4:TextView
+    lateinit var i1:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Initialization
+
+        t1 = findViewById(R.id.txt1)
+        t2 = findViewById(R.id.txt2)
+        t3 = findViewById(R.id.txt3)
+        t4 = findViewById(R.id.txt4)
+        i1 = findViewById(R.id.img1)
+
+        t1.setOnClickListener {
+
+            Toast.makeText(applicationContext,"Text 1",Toast.LENGTH_LONG).show()
+
+            //explicit
+
+            var i = Intent(applicationContext,MainActivity2::class.java)
+            startActivity(i)
+
+        }
+        t2.setOnClickListener {
+            Toast.makeText(applicationContext,"Text 2",Toast.LENGTH_LONG).show()
+
+        }
+        t3.setOnClickListener {
+            Toast.makeText(applicationContext,"Text 3",Toast.LENGTH_LONG).show()
+
+        }
+        t4.setOnClickListener {
+            Toast.makeText(applicationContext,"Text 4",Toast.LENGTH_LONG).show()
+
+        }
+        i1.setOnClickListener {
+
+            Toast.makeText(applicationContext,"Image ",Toast.LENGTH_LONG).show()
+
+            //implicit
+            var url="https://www.tops-int.com/it-training-rajkot"
+            var i = Intent(Intent.ACTION_VIEW)
+            i.setData(Uri.parse(url))
+            startActivity(i)
+        }
+
+
+
+
+    }
+}
