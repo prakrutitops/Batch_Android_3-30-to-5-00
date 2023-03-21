a01dd29e6c2f583ea9d3748d79507d56b2f70d95
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-03-21 16:35:33 +0530

    Add files via upload

---
+++ Module 2/21-3-Material Design/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.example.fragmentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragmentexample.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btn1.setOnClickListener {
+
+                    var f = FirstFragment()
+                    var fm = supportFragmentManager
+                    var ft = fm.beginTransaction()
+                    ft.replace(R.id.frmid,f).commit()
+
+
+        }
+    }
+}
